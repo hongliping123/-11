5bbcd9df9a0ee4344c8b78822a1dfa0eda6b4516
一介码夫 <49940450+hongliping123@users.noreply.github.com>
2020-02-02 04:40:49 +0800

    项目初始化

---
+++ 仿糗事百科/pages/news/news.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		动态sds
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
